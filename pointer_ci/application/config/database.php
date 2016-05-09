674bbc2992b003a31aa01a7858dfb99fdd42ff2e
eji <eji@gra-fik.com>
2016-05-09 14:45:07 +0900

    DB Connect

--- pointer_ci/application/config/database.php
+++ pointer_ci/application/config/database.php
@@ -49,12 +49,12 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'book_user';
+$db['default']['password'] = 'book_password';
+$db['default']['database'] = 'ci_book';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
