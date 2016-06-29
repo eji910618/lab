d3d8b9c6e0e9f5b5556d679ae54e09dac17ddf3f
eji <eji@gra-fik.com>
2016-06-29 17:54:28 +0900

    wide iscroll snap no.1

--- lab/iscroll-wide-carousel.php
+++ lab/iscroll-wide-carousel.php
@@ -25,6 +25,14 @@
 
         .billboard-item {
             float: left;
+            overflow-y: scroll;
+        }
+        .billboard-wide {
+            overflow-y: scroll;
+        }
+        .billboard-wide img {
+            width: auto;
+            height: 500px;
         }
         img {
             display: block;
@@ -35,69 +43,122 @@
 </head>
 <body>
 
-<div class="billboard">
-    <div class="billboard-wrap">
-        <div class="billboard-item">
+<div class="billboard" data-scroll="snap">
+    <div class="billboard-wrap" data-scroll="rail">
+        <div class="billboard-item" data-scroll-current="0" data-scroll="item">
             <img src="/image/wide00.jpg">
         </div>
-        <div class="billboard-item">
+        <div class="billboard-item" data-scroll-current="1" data-scroll="item">
             <img src="/image/wide01.jpg">
         </div>
-        <div class="billboard-item wide">
-            <img src="/image/wide02.JPG">
+        <div class="billboard-item wide" data-scroll-current="2" data-scroll="item">
+            <div class="billboard-wide">
+                <div class="billboard-wide-wrap" style="width: 906px;">
+                    <div class="billboard-wide-item" style="width: 906px;">
+                        <img src="/image/wide02.JPG">
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="billboard-item">
+        <div class="billboard-item" data-scroll-current="3" data-scroll="item">
             <img src="/image/wide03.JPG">
         </div>
-        <div class="billboard-item">
+        <div class="billboard-item" data-scroll-current="4" data-scroll="item">
             <img src="/image/wide04.jpg">
         </div>
     </div>
 </div>
-
-<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script>window.jQuery || document.write('<script src="/js/jquery-1.11.1.min.js"><\/script>')</script>
-<script src="/iscroll/build/iscroll.js"></script>
+<!--<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">-->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<!--<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>-->
+<!--<script src="/iscroll/build/iscroll.js"></script>-->
+<script src="/iscroll/build/iscroll-probe.js"></script>
 <script>
-    var total = 0;
-    function getSlideWidth(target, rail, item) {
-        var itemWidth = target.width();
-        item.each(function() {
-            if($(this).hasClass('wide')) {
-                $(this).css('width', itemWidth*3);
+    function slideWidth() {
+        $('[data-scroll="snap"]').each(function() {
+            var _ = $(this), itemWidth, railWidth,
+                item = _.find('[data-scroll="item"]'),
+                itemLen = item.length;
+            if(_.attr('data-width') || _.hasClass('.horizontal-scroll')) {
+                itemWidth = _.data('width');
+                railWidth = (itemWidth*itemLen+itemLen*10)+20;
             } else {
-                $(this).css('width', itemWidth);
+                itemWidth = _.width();
+                railWidth = itemWidth*itemLen;
             }
-            total += parseInt($(this).width());
+            _.find('[data-scroll="rail"]').css({
+                width: railWidth
+            });
+            item.css({
+                width: itemWidth
+            });
         });
-        rail.css('width', total);
     }
-    getSlideWidth($('.billboard'), $('.billboard .billboard-wrap'), $('.billboard .billboard-item'));
-
+    slideWidth();
     var myScroll = new IScroll('.billboard', {
         scrollX: true,
         scrollY: false,
+        snap: true,
         momentum: false,
-        snap: true
+        eventPassthrough: true
     });
-
-    myScroll.on('scroll', function() {
-        if(this.x <= -(this.wrapperWidth*2)) {
-            this.options.snap = false;
-            this.options.scrollX = false;
-            this.options.momentum = true;
+    var wideScroll = new IScroll('.billboard-wide', {
+        scrollX: true,
+        scrollY: false,
+        probeType: 3,
+        bounce: false
+    });
+    var scrollDirection, wideScrollDirection;
+    myScroll.on('scrollStart', function(){
+        scrollDirection = this.directionX;
+    });
+    wideScroll.on('scrollStart', function() {
+        wideScrollDirection = this.directionX;
+//        console.log(wideScrollDirection);
+    });
+    wideScroll.on('scrollEnd', function() {
+        if ( wideScrollDirection == -1 && wideScroll.x == 0 ) {
+            myScroll.enable();
+            wideScroll.disable();
         }
-       /* else {
-            console.log("end!!");
-            this.options.snap = true;
-            this.options.scrollX = true;
-        }*/
-        console.log(this.x);
-        console.log(this.options.snap);
+        if ( wideScrollDirection == 1 && wideScroll.x == wideScroll.maxScrollX ) {
+            myScroll.enable();
+            wideScroll.disable();
+        }
     });
+    /*if ( wideScroll.x == wideScroll.maxScrollX ) {
+    $('.billboard-wide').on('swipeleft', function(){
+            myScroll.enable();
+            wideScroll.disable();
+    });
+    }
+    $('.billboard-wide').on('swiperight', function(){
+        if ( wideScroll.x == 0 ) {
+            myScroll.enable();
+            wideScroll.disable();
+        }
+    });*/
+    myScroll.on('scrollEnd', function(){
+        addActiveClass(this, '.billboard');
+        if ( scrollDirection == 1 && $('.billboard .billboard-item.active').hasClass('wide') ) {
+            myScroll.disable();
+            wideScroll.enable();
+        }
+        if ( scrollDirection == -1 && $('.billboard .billboard-item.active').hasClass('wide') ) {
+            myScroll.disable();
+            wideScroll.enable();
+        }
+    });
+    function addActiveClass(contentsScroll, target) {
+        var scrollEndPage = contentsScroll.currentPage.pageX,
+            activeLink = $(target + ' .billboard-item[data-scroll-current="' + scrollEndPage + '"]');
+        $(target + ' .billboard-item').removeClass('active');
+        $(activeLink).addClass('active');
+    }
+    addActiveClass(myScroll, '.billboard');
 
     $(window).resize(function() {
-        getSlideWidth($('.billboard'), $('.billboard .billboard-wrap'), $('.billboard .billboard-item'));
+        slideWidth();
     });
 
 </script>
